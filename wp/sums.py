1d3a00712805377311f0b99eb65d2e3e33dcbad5
jjsutula <jsutula@workpoint.com>
2019-05-01 13:12:18 -0500

    Initial commit

---
+++ wp/sums.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def compute_sums():
+  
+
+
+def main(argv):
+  compute_sums()
+
+# Do this so that main() only gets called if invoked via command line, but not if invoked programmatically
+if __name__ == "__main__":
+  main(sys.argv[1:])
